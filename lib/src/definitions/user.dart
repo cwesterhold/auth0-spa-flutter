2bbfec2a5a31e23d427e8e9a409433e87640b4e5
cwesterhold <chris@liquidsoft>
2021-04-13 20:03:48 -0500

    return User dart object

---
+++ lib/src/definitions/user.dart
@@ -0,0 +1,15 @@
+class User {
+  final String name;
+  final String nickname;
+  final String picture;
+  final String sub;
+  final String updated_at;
+
+  const User({
+    this.sub,
+    this.name,
+    this.picture,
+    this.nickname,
+    this.updated_at,
+  });
+}
--- lib/src/js/_auth0_client_js.dart
+++ lib/src/js/_auth0_client_js.dart
@@ -7,7 +7,7 @@ import 'package:auth0_spa/src/definitions/id_token.dart';
 import 'package:auth0_spa/src/definitions/logout_options.dart';
 import 'package:auth0_spa/src/definitions/popup_login_options.dart';
 import 'package:auth0_spa/src/definitions/redirect_login_options.dart';
-import 'package:auth0_spa/src/definitions/redirect_login_result.dart';
+import 'package:auth0_spa/src/definitions/user.dart';
 import 'package:auth0_spa/src/js/utility/js_object_bridge.dart';
 
 import 'utility/js_object_bridge.dart';
@@ -22,20 +22,27 @@ class Auth0ClientJs extends Auth0Client {
 
   @override
   void loginWithRedirect([RedirectLoginOptions options]) {
-    this._jsAuth0Client.invokeAsyncMethod<void>("loginWithRedirect", <dynamic>[options?.toJsObject()]);
+    this
+        ._jsAuth0Client
+        .invokeAsyncMethod<void>("loginWithRedirect", <dynamic>[options?.toJsObject()]);
   }
 
   @override
   Future<void> loginWithPopup([PopupLoginOptions options]) {
-    return this._jsAuth0Client.invokeAsyncMethod<void>("loginWithPopup", <dynamic>[options?.toJsObject()]);
+    return this
+        ._jsAuth0Client
+        .invokeAsyncMethod<void>("loginWithPopup", <dynamic>[options?.toJsObject()]);
   }
 
   @override
-  Future<Map<String, String>> handleRedirectCallbackWithQuery(String searchQuery, {List<String> appStateKeys = const []}) async {
+  Future<Map<String, String>> handleRedirectCallbackWithQuery(String searchQuery,
+      {List<String> appStateKeys = const []}) async {
     if (searchQuery.startsWith("/")) {
       searchQuery = searchQuery.substring(1);
     }
-    final result = await this._jsAuth0Client.invokeAsyncMethod<JsObjectBridge>("handleRedirectCallback", <dynamic>[searchQuery]);
+    final result = await this
+        ._jsAuth0Client
+        .invokeAsyncMethod<JsObjectBridge>("handleRedirectCallback", <dynamic>[searchQuery]);
     final appState = result.get<JsObjectBridge>("appState");
     var convertedAppState = <String, String>{};
     for (String key in appStateKeys) {
@@ -45,14 +52,29 @@ class Auth0ClientJs extends Auth0Client {
   }
 
   @override
-  Future<dynamic> getUser([GetUserOptions options]) {
-    return this._jsAuth0Client.invokeAsyncMethod<dynamic>("getUser", <dynamic>[options?.toJsObject()]);
+  Future<dynamic> getUser([GetUserOptions options]) async {
+    JsObjectBridge userJs =
+        await this._jsAuth0Client.invokeAsyncMethod("getUser", <dynamic>[options?.toJsObject()]);
+
+    try {
+      return User(
+        sub: userJs.get("sub"),
+        name: userJs.get("name"),
+        nickname: userJs.get("nickname"),
+        picture: userJs.get("picture"),
+        updated_at: userJs.get("updated_at"),
+      );
+    } catch (e) {
+      return null;
+    }
   }
 
   @override
   Future<String> getTokenSilently([GetTokenSilentlyOptions options]) async {
     try {
-      final result = await this._jsAuth0Client.invokeAsyncMethod<String>("getTokenSilently", <dynamic>[options?.toJsObject()]);
+      final result = await this
+          ._jsAuth0Client
+          .invokeAsyncMethod<String>("getTokenSilently", <dynamic>[options?.toJsObject()]);
       return result;
     } catch (e) {
       if (e.error != null && e.error == "login_required") {
@@ -66,7 +88,9 @@ class Auth0ClientJs extends Auth0Client {
 
   @override
   Future<IdToken> getIdTokenClaims([GetIdTokenClaimsOptions options]) async {
-    JsObjectBridge userTokenJs = await this._jsAuth0Client.invokeAsyncMethod("getIdTokenClaims", <dynamic>[options?.toJsObject()]);
+    JsObjectBridge userTokenJs = await this
+        ._jsAuth0Client
+        .invokeAsyncMethod("getIdTokenClaims", <dynamic>[options?.toJsObject()]);
     return IdToken(
       rawJwt: userTokenJs.get("__raw"),
       email: userTokenJs.get("email"),
@@ -79,7 +103,9 @@ class Auth0ClientJs extends Auth0Client {
 
   @override
   Future<String> getTokenWithPopup([GetTokenWithPopupOptions options]) {
-    return this._jsAuth0Client.invokeAsyncMethod("getTokenWithPopup", <dynamic>[options?.toJsObject()]);
+    return this
+        ._jsAuth0Client
+        .invokeAsyncMethod("getTokenWithPopup", <dynamic>[options?.toJsObject()]);
   }
 
   @override
